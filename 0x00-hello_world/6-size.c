365b7603e11b429a91e2e58e70c2c4d87d6b655f
fetuga-ayisat <fetugaayisat65@gmail.com>
2022-09-08 16:40:52 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", size(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
-	return(0);
+	return (0);
 }
 	
