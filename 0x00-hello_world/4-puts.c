a8c47dbbb98de1afd794f414456ae7d8013ee9c5
fetuga-ayisat <fetugaayisat65@gmail.com>
2022-09-08 00:57:11 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,7 @@
 #include <stdio.h>
 
-
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
 int main(void)
-
 {
-
-		char test_str[] = "\"Programming is like building a multilingual puzzle";
-
-
-
-			puts(test_str);
-
-				return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
