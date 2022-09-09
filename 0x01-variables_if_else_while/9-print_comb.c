b9413f979d549032a5498fd8947118dd4a28e1e1
fetuga-ayisat <fetugaayisat65@gmail.com>
2022-09-09 08:22:45 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all possibles combinations of single digit numbers.
+ (*
+ * Return: 0  on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+		putchar(',');
+	putchar(' ');
+		}
+	++1;
+	}
+putchar('\n');
+return (0);
+
+}
