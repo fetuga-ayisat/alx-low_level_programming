58969651ce7b9abcb877b97a343312e3c6c10e86
Fetuga Ayisat <83713768+fetuga-ayisat@users.noreply.github.com>
2022-09-14 01:14:21 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			_putchar(i / 10) + 48);
+			_putchar(i / 10 + 48);
 			_putchar(i % 10 + 48);
 			_putchar(':');
 			_putchar(j / 10 + 48);
