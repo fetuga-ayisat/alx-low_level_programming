f7e4fc468a8b63deef3d9dceec08c55cd4ff77ae
fetuga-ayisat <fetugaayisat65@gmail.com>
2022-09-14 02:55:23 -0700

    103

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
+ * Numbers must be coma and space separated.
+ * Return: 0
+ */
+int main(void)
+{
+	int count = 2;
+	long in i 
