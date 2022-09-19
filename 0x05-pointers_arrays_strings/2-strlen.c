22446c23055611b55802ed9fd925c79ffefa87ef
fetuga-ayisat <fetugaayisat65@gmail.com>
2022-09-19 08:49:09 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int 1 = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (1);
+}
