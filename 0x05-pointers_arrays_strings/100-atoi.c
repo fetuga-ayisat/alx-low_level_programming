a952317eb5612670cabc423ab6522e48793beb10
Fetuga Ayisat <fetugaayisat65@gmail.com>
2022-09-19 18:43:36 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,29 +1,28 @@
 #include "holberton.h"
+
 /**
- * _atoi - int
- * @s: pointer
- * Return: int.
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
  */
+
 int _atoi(char *s)
 {
-	int i;
-	int res = 0;
-	int sig = -1;
-	int brk = 0;
+	int sign = 1, resp = 0, firstNum;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
-	if (s[i] == '-')
-	sig = sig * -1;
-	if (s[i] >= '0' && s[i] <= '9')
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
-	res = res * 10;
-res -= (s[i] >= '0');
-brk = 1;
+		resp *= 10;
+		resp += (s[i] - 48);
 	}
-	else if (brk == 1)
-		break;
-	}
-	res = sig * res;
-	return (res);
+
+	return (sign * resp);
 }
