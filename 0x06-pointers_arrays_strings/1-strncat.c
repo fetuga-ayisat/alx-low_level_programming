8332b9114036e0791347469d763cdaa77677a2eb
Fetuga Ayisat <fetugaayisat65@gmail.com>
2022-09-21 21:49:21 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -25,10 +25,9 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
-	{dest[m +i] = src[i];
+	{
+		dest[m + i] = src[i];
 	}
-	dest[m + i] = src[i];
-}
-dest[m + i] = '\0';
-return (dest);
+	dest[m + i] = '\0';
+	return (dest);
 }
