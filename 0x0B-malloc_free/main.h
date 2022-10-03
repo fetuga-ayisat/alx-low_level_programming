377084e17acae1a93e32876a3fe46235e8cdd0cb
Fetuga Ayisat <fetugaayisat65@gmail.com>
2022-10-03 10:38:18 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,5 @@
-#if !defined(MAIN_H)
-#define MAIN_H
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -10,5 +7,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-char **strtow(char *str);
+
 #endif
