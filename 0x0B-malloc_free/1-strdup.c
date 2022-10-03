36d5c9f64080b0eb18167f98a01754c2e625e7b7
fetuga-ayisat <fetugaayisat65@gmail.com>
2022-10-03 05:17:43 -0700

    0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 #include <stdlib.h>
 /**
  * _strdup - returns a pointer to a newly allocated space in memory.
@@ -7,23 +8,43 @@
  * Return: pointer of an array of chars
  */
 char *_strdup(char *str)
+
 {
+
 	char *strout;
+
 	unsigned int i, j;
 
+
+
 	if (str == NULL)
+
 		return (NULL);
 
+
+
 	for (i = 0; str[i] != '\0'; i++)
+
 		;
+
+
 
 	strout = (char *)malloc(sizeof(char) * (i + 1));
 
+
+
 	if (strout == NULL)
+
 		return (NULL);
 
+
+
 	for (j = 0; j <= i; j++)
+
 		strout[j] = str[j];
 
+
+
 	return (strout);
+
 }
